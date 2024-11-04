7c290d5657fc54d9cbda4c58f5f2ffb06ec31c58
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-04 20:30:07 +0500

    first commit

---
+++ src/main/java/me/mutashim/votesmart/VoteSmartBackendApplication.java
@@ -0,0 +1,13 @@
+package me.mutashim.votesmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSmartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteSmartBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mutashim/votesmart/VoteSmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mutashim.votesmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSmartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
