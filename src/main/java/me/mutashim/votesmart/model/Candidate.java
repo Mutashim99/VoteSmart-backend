34f7c956dcb85fe0d8c595e2c1f0f1cfa82aeb5c
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2024-11-19 00:26:09 +0500

    remove age from candidate for more broader use of this project

--- src/main/java/me/mutashim/votesmart/model/Candidate.java
+++ src/main/java/me/mutashim/votesmart/model/Candidate.java
@@ -4,17 +4,17 @@ public class Candidate {
 
     private String id;
     private String name;
-    private int age;
+
     private String description;
     private int voteCount;
 
 
     public Candidate() {}
 
-    public Candidate(String id, String name, int age, String description, int voteCount) {
+    public Candidate(String id, String name, String description, int voteCount) {
         this.id = id;
         this.name = name;
-        this.age = age;
+
         this.description = description;
         this.voteCount = voteCount;
     }
@@ -35,15 +35,6 @@ public class Candidate {
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getDescription() {
         return description;
     }
